7d602bcaa3555252df0e6cde044d6d884fb95ae4
SGIvanov <silviu.ivanov@yahoo.com>
2023-12-20 07:41:40 +0200

    Add docker and tailwind

--- portofolio-client-app/src/App.js
+++ portofolio-client-app/src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="bg-red-600">
+          Edit <code>src/App.js</code> and save to reload docker me baby.
         </p>
         <a
           className="App-link"
